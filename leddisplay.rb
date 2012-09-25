9a468a4ae451d50b59b7b5bdc214e74460e05679
Daniel Zwijnenburg <daniel@danielz.nl>
2012-09-25 17:41:06 +0200

    Little bit
    Just to show some ruby tricks :)

--- leddisplay.rb
+++ leddisplay.rb
@@ -1,46 +1,42 @@
 class LedDisplay
 
-	Numbers = [
-		[" _ ", 
-		 "| |", 
-		 "|_|"], 
-		["   ", 
-		 "  |",
-		 "  |"], 
-		[" _ ", 
-		 " _|", 
-		 "|_ "], 
-		[" _ ",
-		 " _|",
-		 " _|"], 
-		["   ",
-		 "|_|",
-		 "  |"], 
-		[" _ ",
-		 "|_ ",
-		 " _|"], 
-		[" _ ",
-		 "|_ ",
-		 "|_|"], 
-		[" _ ",
-		 "  |",
-		 "  |"], 
-		[" _ ",
-		 "|_|",
-		 "|_|"], 
-		[" _ ",
-		 "|_|",
-		 " _|"]]
+		Numbers = [
+			[" _ ", 
+			 "| |", 
+			 "|_|"], 
+			["   ", 
+			 "  |",
+			 "  |"], 
+			[" _ ", 
+			 " _|", 
+			 "|_ "], 
+			[" _ ",
+			 " _|",
+			 " _|"], 
+			["   ",
+			 "|_|",
+			 "  |"], 
+			[" _ ",
+			 "|_ ",
+			 " _|"], 
+			[" _ ",
+			 "|_ ",
+			 "|_|"], 
+			[" _ ",
+			 "  |",
+			 "  |"], 
+			[" _ ",
+			 "|_|",
+			 "|_|"], 
+			[" _ ",
+			 "|_|",
+			 " _|"]]
 
 	def convert(number)
-		result = []
-
-		3.times do |lineNr|
+		result = 3.times.inject([]) do |array, lineNr|
 			digits = number.to_s.chars.inject([]) { |array, digit| array << digit.to_i }
 			line = digits.inject([]) { |array, digit| array << Numbers[digit][lineNr] }
-			result << line.join(" ")
-		end
-
-		result.join("\n")
+			array << line.join(" ")
+		end.join("\n")
 	end
 end
